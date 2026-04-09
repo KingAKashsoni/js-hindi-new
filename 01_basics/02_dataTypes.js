50d5c6d35aff55752d623727688173fd216a50a9
Akash Soni <150895678+KingAKashsoni@users.noreply.github.com>
2026-04-09 08:51:05 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+ "use strict"; // treat all JS code as newer version
+
+//  alert(3 + 4) // we are using nodejs, not browser
+
+// code readbility should be high
+
+console.log(3 + 3) 
+
+let name = "akash"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//<-- primitivs data type -->
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symble 
+
+
+//<-- objects -->
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
